0802f734833d90d446f5a4b9711eca6820c0e9f1
amdestefano <61249904+anthony-destefano@users.noreply.github.com>
2021-04-20 17:38:02 -0500

    Runs complete

--- ComputeBragg.py
+++ ComputeBragg.py
@@ -6,7 +6,26 @@ import matplotlib.pyplot as plt
 
 import glob
 
-file_dir = 'C:\Users\adestefa\Documents\Code\SRIM-2013\SRIM Outputs\SolarCruiserRCD'
+#file_dir = 'C:\Users\adestefa\Documents\Code\SRIM-2013\SRIM Outputs\SolarCruiserRCD'
 
-for name in glob.glob(file_dir + '\IONIZ_Bragg*'):
-    print(name)
+E = np.logspace(2., np.log10(4096.E2), 13)
+i = 0
+
+peaks = np.zeros(13)
+
+for filename in glob.glob('IONIZ_Bragg*'):
+    print(filename)
+
+    depth, E_ion, E_recoil = np.loadtxt(filename, unpack=True, skiprows=26)
+
+    peak_idx = np.argmax(E_ion+E_recoil)
+
+    peaks[i] = depth[peak_idx]
+    i = i+1
+    # plt.loglog(depth, E_ion)
+    # plt.show()
+
+data = np.array([E, peaks])
+data = data.T
+
+np.savetxt('peak_vs_E.txt', data, delimiter=' ')
