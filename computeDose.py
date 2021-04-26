b1628e114f0f6ffb33ceae1956d31cdd26134a5b
amdestefano <61249904+anthony-destefano@users.noreply.github.com>
2021-04-26 12:26:45 -0500

    Update figures

--- computeDose.py
+++ computeDose.py
@@ -13,13 +13,14 @@ energyCenterFile = './' + fileDirectory + '/' + sys.argv[2]
 angleCenterFile  = './' + fileDirectory + '/' + sys.argv[3]
 density = float(sys.argv[4]) # kg/cm^3
 
-energy, flux, Ebin_width     = np.loadtxt(energyCenterFile, unpack=True)
-angle , weight, angbin_width = np.loadtxt(angleCenterFile , unpack=True)
+energy, flux, Ebin_width, Ebin_left = np.loadtxt(energyCenterFile, unpack=True)
+angle , weight, angbin_width        = np.loadtxt(angleCenterFile , unpack=True)
 
-NE    = energy.size
+NE     = energy.size
 Ntheta = angle.size
+Ndepth = 100
 
-dose_depth  = np.zeros(100)
+dose_depth  = np.zeros(Ndepth)
 dose_energy = np.zeros(NE)
 dose_angle  = np.zeros(Ntheta)
 
@@ -45,23 +46,54 @@ for i in range(0, NE):
 
 dose_tot = np.sum(dose_energy)
 
+# compute integral dose vs energy
+int_dose_E = np.zeros(NE)
+for i in range(0, NE):
+	int_dose_E[i] = np.sum(dose_energy[i:NE-1])
 
+# compute integral dose vs depth
+int_dose_depth = np.zeros(Ndepth)
+for i in range(0, Ndepth):
+	int_dose_depth[i] = np.sum(dose_depth[0:i])
+
+plt.loglog(Ebin_left, int_dose_E)
+plt.title('Integral Dose vs. Energy\nTotal Dose = ' + str(np.round(dose_tot/1000.)) + ' krads')
+plt.xlabel('Energy (keV)')
+plt.ylabel('Integral Dose (rad > Energy)')
+plt.grid(b=True, which='both') # https://stackoverflow.com/questions/9127434/how-to-create-major-and-minor-gridlines-with-different-linestyles-in-python
+plt.savefig(fileDirectory + "_Integral_Dose_vs_Energy.png", dpi=800)
+
+plt.figure()
 plt.loglog(energy, dose_energy/Ebin_width)
-plt.title('Dose vs. Energy\nTotal Dose = ' + str(np.round(dose_tot/1000.)) + ' krads')
+plt.title('Differential Dose vs. Energy\nTotal Dose = ' + str(np.round(dose_tot/1000.)) + ' krads')
 plt.xlabel('Energy (keV)')
 plt.ylabel('Dose (rad/keV)')
+plt.grid(b=True, which='both') # https://stackoverflow.com/questions/9127434/how-to-create-major-and-minor-gridlines-with-different-linestyles-in-python
+plt.savefig(fileDirectory + "_Differential_Dose_vs_Energy.png", dpi=800)
+
+plt.figure()
+plt.loglog(depth/10000., int_dose_depth)
+plt.title('Integral Dose vs. Depth\nTotal Dose = ' + str(np.round(dose_tot/1000.)) + ' krads')
+plt.xlabel('Depth (microns)')
+plt.ylabel('Integral Dose (rad < Depth)')
+plt.grid(b=True, which='both') # https://stackoverflow.com/questions/9127434/how-to-create-major-and-minor-gridlines-with-different-linestyles-in-python
+plt.savefig(fileDirectory + "_Integral_Dose_vs_Depth.png", dpi=800)
 
 plt.figure()
 plt.loglog(depth/10000., dose_depth/(depth[0]/10000.))
-plt.title('Dose vs. Depth\nTotal Dose = ' + str(np.round(dose_tot/1000.)) + ' krads')
+plt.title('Differential Dose vs. Depth\nTotal Dose = ' + str(np.round(dose_tot/1000.)) + ' krads')
 plt.xlabel('Depth (microns)')
 plt.ylabel('Dose (rad/micron)')
+plt.grid(b=True, which='both') # https://stackoverflow.com/questions/9127434/how-to-create-major-and-minor-gridlines-with-different-linestyles-in-python
+plt.savefig(fileDirectory + "_Differential_Dose_vs_Depth.png", dpi=800)
 
 plt.figure()
 plt.plot(angle, dose_angle/(angbin_width*np.pi/180. * 2*np.pi))
-plt.title('Dose vs. Angle\nTotal Dose = ' + str(np.round(dose_tot/1000.)) + ' krads')
+plt.title('Differential Dose vs. Angle\nTotal Dose = ' + str(np.round(dose_tot/1000.)) + ' krads')
 plt.xlabel('Incident Angle (degrees)')
 plt.ylabel('Dose (rad/steradians)')
+plt.grid(b=True, which='both') # https://stackoverflow.com/questions/9127434/how-to-create-major-and-minor-gridlines-with-different-linestyles-in-python
+plt.savefig(fileDirectory + "_Differential_Dose_vs_Angle.png", dpi=800)
 
 plt.show()
 
